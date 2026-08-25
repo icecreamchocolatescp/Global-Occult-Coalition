c5e848c437b8d3b78916c43fb07ca0f256876e66
icecreamchocolatescp <debarghapramanik008@gmail.com>
2026-08-25 22:54:44 +0530

    Update bot activity status message

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,8 +23,8 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
-        state: "Watching Global Occult Coalition",     // this is what people actually see
+        name: "Watching Global Occult Coalition", // required by Discord API, not shown in the client
+        state: "MEOW PURR",     // this is what people actually see
         type: 3,               // Custom
       },
     ],
